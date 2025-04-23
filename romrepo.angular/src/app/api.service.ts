ab88b57075da3760be58b0541e55a4ec2190e2f2
Shawn Renner <shawn.renner@gmail.com>
2025-04-22 21:37:28 -0500

    use env variables for api url

--- romrepo.angular/src/app/api.service.ts
+++ romrepo.angular/src/app/api.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { SystemSetting } from './interface/system-setting';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,12 @@ import { Observable } from 'rxjs';
 export class ApiService {
 
   private dotNetDebuggerPath:string = 'http://localhost:62746/api/';
-  private servicePath:string = 'http://localhost/api/';
+  private servicePath:string = 'http://localhost:5000/api/';
 
   private http = inject(HttpClient);
-  private BASE_PATH:string = this.dotNetDebuggerPath;
+  private BASE_PATH:string = environment.apiUrl;
 
-  constructor() { }
+  constructor() {}
 
 
     getSystemSettings():Observable<any> {
